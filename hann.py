bb4668724e34b97550a3dde18eb1fa587a0a3e6a
Anthony Morast <anthonymorast33@hotmail.com>
2018-03-21 13:54:55 -0600

    results etc.

--- hann.py
+++ hann.py
@@ -16,6 +16,8 @@ def create_lookback_dataset(trainx, lookback):
         Builds arrays containing previous information for the x inputs.
     """
     datax, datay = [], []
+    if lookback == 0:
+        lookback = 1
     for i in range(len(trainx) - lookback):
         datax.append(trainx[i:i+lookback])
         datay.append(trainx[i + lookback])
@@ -37,8 +39,8 @@ def train_networks(trainx, trainy):
         x, y = trainx[i], trainy[i]
 
         print("Training network", i + 1, "out of", len(trainx), "...")
-        networks.append([MyLSTM(x.shape[1], 5, [10 for _ in range(10)], 1, epochs=550,
-                            batch_size=200, fit_verbose=2, variables=x.shape[2]),
+        networks.append([MyLSTM(x.shape[1], 4, [27, 25, 3, 45], 1, epochs=1850,
+                            batch_size=100, fit_verbose=2, variables=x.shape[2]),
                             calc_variance(x)])
         networks[i][0].train(x, y)
     return networks
@@ -46,8 +48,8 @@ def train_networks(trainx, trainy):
 
 if __name__ == '__main__':
     trainsize = 2000
-    look_back = 300
-    num_chunks = 3
+    look_back = 1
+    num_chunks = 2
     chunk_size = int(trainsize / num_chunks)
 
     dh = DataHandler('./data/Sunspots.csv')
@@ -58,20 +60,19 @@ if __name__ == '__main__':
 
     testsize = len(data) - (trainsize + look_back)
     trainx = x[:trainsize + look_back]
-    testx, testy = x[(trainsize + 1):], y[(trainsize + 1):]
-    # testx, testy = testx.tolist(), testy.tolist()
+    testx, testy = x[(trainsize + look_back - 1):], y[(trainsize + look_back - 1):]
 
     trainx, trainy = create_lookback_dataset(trainx, look_back)
     trainx = chunk_data(trainx, chunk_size)
     trainy = chunk_data(trainy, chunk_size)
     networks = train_networks(trainx, trainy)
 
-    testx, _ = create_lookback_dataset(testx, look_back)
+    testx, testy = create_lookback_dataset(testx, look_back)
     histx, histy = [], []
     predictions = []
     # TODO: Different window now since lookback
     var_window = x[(trainsize-100):]
-    for i in range(100, testsize):
+    for i in range(testsize):
         xp, yp = testx[i], testy[i]
         histx.append(xp)
         histy.append(yp)
@@ -81,10 +82,14 @@ if __name__ == '__main__':
             histx, histy = [], []
         prediction = 0
         for nn in networks:
-            prediction += nn[0].predict(xp)
-            print(prediction, yp)
+            p = nn[0].predict(xp)
+            prediction += p.reshape(1)
+        prediction /= len(networks)
+        predictions.append(prediction.tolist())
+        # print(prediction, yp, len(networks))
 
-    print(mse(predictions, testy))
+    print(predictions)
+    print(mse(testy.tolist(), predictions))
     """
         TODO:
             - break data up into chunks (size? <- part of the problem)
